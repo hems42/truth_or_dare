730f40c6a0abd66d45273ba95b49d929df81fe40
caglar <eflatun126@gmail.com>
2021-06-30 23:25:25 +0300

    .

---
+++ src/main/java/com/truth_or_dare/TruthOrDareApplication.java
@@ -0,0 +1,27 @@
+package com.truth_or_dare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class TruthOrDareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruthOrDareApplication.class, args);
+	}
+
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("cap_software.hrms"))
+				.build();
+	}
+}
---
+++ src/test/java/com/truth_or_dare/TruthOrDareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truth_or_dare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruthOrDareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
